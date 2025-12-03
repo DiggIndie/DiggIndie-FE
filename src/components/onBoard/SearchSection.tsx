1a602f5afe739c569f82a95f60f924263e742455
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-12-03 15:45:08 +0900

    [#1] style: 온보딩 아티스트 선택페이지 오버레이 스타일링

--- src/components/onBoard/SearchSection.tsx
+++ src/components/onBoard/SearchSection.tsx
@@ -5,7 +5,7 @@ export default function SearchSection() {
   return (
     <section className="w-full flex p-2 bg-gray-700 rounded- flex gap-3">
       <Image src={searchIcon} alt="돋보기 아이콘" />
-      <input placeholder="검색어를 입력하세요" />
+      <input placeholder="검색어를 입력하세요" className="w-full" />
     </section>
   );
 }
