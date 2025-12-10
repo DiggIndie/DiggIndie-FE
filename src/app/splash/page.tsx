43b51e79c69e33a351b90c3db7d1549efbd73b70
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-12-10 15:56:03 +0900

    [#5] feat: 스플래시 애니메이션 추가

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -8,20 +8,24 @@ import Diggindie from '@/assets/common/diggindie.svg';
 export default function SplashPage() {
   const router = useRouter();
   const [step, setStep] = useState(0);
+  const [fade, setFade] = useState(true);
 
   useEffect(() => {
-    // 1초 후 step을 1로 변경
-    const changeStep = setTimeout(() => {
-      setStep(1);
+    const toStep1 = setTimeout(() => {
+      setFade(false); // fade-out
+
+      setTimeout(() => {
+        setStep(1);
+        setFade(true); // fade-in
+      }, 300);
     }, 1000);
 
-    // 2초 후 홈으로 이동
     const movePage = setTimeout(() => {
       router.push('/');
-    }, 2000);
+    }, 5000);
 
     return () => {
-      clearTimeout(changeStep);
+      clearTimeout(toStep1);
       clearTimeout(movePage);
     };
   }, [router]);
@@ -30,14 +34,28 @@ export default function SplashPage() {
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-[#140B0B] to-[#330301] gap-4 px-6 text-center">
       {step === 0 ? (
         <>
-          <Image src={Diggindie} alt="logo" width={180} />
-          <span className="text-base font-medium text-white/80">
+          <Image
+            src={Diggindie}
+            alt="logo"
+            width={180}
+            className={`transition-opacity duration-700 ${fade ? 'opacity-100' : 'opacity-0'}`}
+          />
+
+          <span
+            className={`text-base font-medium text-white/80 transition-opacity duration-700 ${
+              fade ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
             더 깊고 끝없는 음악 여정이 시작됩니다.
           </span>
         </>
       ) : (
         <>
-          <span className="text-base font-medium text-white/80 leading-relaxed">
+          <span
+            className={`text-base font-medium text-white/80 transition-opacity duration-700 ${
+              fade ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
             취향에 맞는 인디뮤직 디깅을 편리하게,
             <br />
             공연 정보부터 뮤지션 발굴까지, 모든 것을 한 곳에서
