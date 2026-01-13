057a6628e3c5c7a2107fcbb64b5d6ee4478e738e
백승선 <baekjimmy@yonsei.ac.kr>
2026-01-13 17:39:46 +0900

    fix: 취향 재설정 반응형 적용

--- src/components/home/ResetPreference.tsx
+++ src/components/home/ResetPreference.tsx
@@ -1,4 +1,5 @@
-'use-client';
+'use client';
+
 import next from '@/assets/common/more.svg';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -10,16 +11,18 @@ type Props = {
 export default function ResetPreference({ isLoggedIn }: Props) {
   const router = useRouter();
   if (!isLoggedIn) return null;
+
   return (
     <div
-      className={`w-[335px] h-[46px] flex bg-[#FF3637] mt-[40px] items-center rounded-[4px] cursor-pointer`}
+      className="flex w-full max-w-[335px] h-[46px] bg-[#FF3637] mt-[40px] items-center rounded-[4px] cursor-pointer"
       onClick={() => router.push('/onboard/artist')}
     >
-      <div className={`flex overflow-x-auto text-[16px] ml-[12px] font-semibold`}>
-        <span className={'w-[148px] h-[22px] ml-[12px]'}>취향 재설정 하러 가기</span>
+      <div className="min-w-0 grow-0 shrink text-[16px] ml-[12px] font-semibold">
+        <span className="block truncate">취향 재설정 하러 가기</span>
       </div>
-      <div className={'w-[16px] h-[16px]'}>
-        <Image src={next} alt={next} />
+
+      <div className="w-[16px] h-[16px] ml-[4px] shrink-0 flex items-center justify-center">
+        <Image src={next} alt="next" width={16} height={16} />
       </div>
     </div>
   );
