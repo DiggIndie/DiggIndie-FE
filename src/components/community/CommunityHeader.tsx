87a58d2ab4024bde73fa5970783f25a6da9b0fc9
백승선 <baekjimmy@yonsei.ac.kr>
2026-01-21 17:15:03 +0900

    fix: 커뮤니티 헤더 라우팅

--- src/components/community/CommunityHeader.tsx
+++ src/components/community/CommunityHeader.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import searchBtn from '@/assets/common/search.svg'
 import writeBtn from '@/assets/common/write.svg'
 import menuBtn from '@/assets/common/hamburger.svg'
+import { useRouter } from 'next/navigation';
 
 type props = {
   title: string;
@@ -10,13 +11,15 @@ type props = {
 };
 
 export default function CommunityHeader({ title, onHamburgerClick }: props) {
-
+  const router = useRouter();
   return (
     <div className="bg-black w-full h-[52px] flex items-center px-5 py-3 top-0 z-50 justify-between">
       <span className="text-[20px] font-semibold">{title}</span>
       <div className={"flex gap-[10px]"}>
         <Image src={searchBtn} alt={"search"} width={24} height={24} className={'cursor-pointer'} />
-        <Image src={writeBtn} alt={"write"} width={24} height={24} className={'cursor-pointer'} />
+        <div onClick={() => router.push('/community/write')}>
+          <Image src={writeBtn} alt={"write"} width={24} height={24} className={'cursor-pointer'} />
+        </div>
         <Image src={menuBtn} alt={"menu"} width={24} height={24} className={'cursor-pointer'} onClick={onHamburgerClick} />
       </div>
     </div>
