e1d2c23a2e0aade5f44150ed69f99744faec0375
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2026-01-28 15:42:33 +0900

    [#66] feat: 홈 로그인 시 로고 위치에 아이디 띄우기

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,21 +13,34 @@ import { useEffect, useState } from 'react';
 import SideTab from '@/components/sideTabDir/SideTab';
 import { useAuthStore } from '@/stores/authStore';
 import Feedback from '@/components/home/Feedback';
+import { authService } from '@/services/authService';
 
 export default function HomePage() {
   const { isAuthed } = useAuthStore();
-
+  const userId = useAuthStore((s) => s.userId);
   const [isSideTabOpen, setIsSideTabOpen] = useState(false);
 
   useEffect(() => {
     document.body.style.overflow = isSideTabOpen ? 'hidden' : 'auto';
   }, [isSideTabOpen]);
 
+  useEffect(() => {
+    const fetchIdIfNeeded = async () => {
+      // 로그인은 되어 있는데 스토어에 userId가 없다면 (새로고침 상황 등)
+      if (isAuthed && !userId) {
+        try {
+          await authService.getUserId();
+        } catch (error) {
+          console.error('사용자 정보를 가져오는 데 실패했습니다.');
+        }
+      }
+    };
+  });
   return (
     <div className="text-white min-h-screen bg-black relative">
       <div className="flex flex-col">
         <main className="flex flex-col items-center bg-black pb-20">
-          <HomeHeader onHamburgerClick={() => setIsSideTabOpen(true)} />
+          <HomeHeader onHamburgerClick={() => setIsSideTabOpen(true)} userId={userId} />
           <IndieStoryRec />
           <div className="px-5 w-full">
             <LoginBanner isLoggedIn={isAuthed} />
